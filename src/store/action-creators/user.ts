50d6448bd0d40920fa6ee57f93d0fd65ea9a4363
Dem0nZ <cbpskov@gmail.com>
2021-02-07 01:46:00 +0300

    add first reducer&store

---
+++ src/store/action-creators/user.ts
@@ -0,0 +1,21 @@
+import {UserAction, UserActionTypes} from "../../types/todo";
+import {Dispatch} from "redux";
+import axios from "axios";
+
+
+export const fetchUsers = () => {
+    return async (dispatch: Dispatch<UserAction>) => {
+        try {
+            dispatch({type: UserActionTypes.FETCH_USERS})
+            const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+            setTimeout(()=>{
+                dispatch({type: UserActionTypes.FETCH_USERS_SUCCESS, payload: response.data})
+            },1000)
+        } catch (e) {
+            dispatch({
+                type: UserActionTypes.FETCH_USERS_ERROR,
+                payload: 'Pizda rulu. юзеры сбежали'
+            })
+        }
+    }
+}
